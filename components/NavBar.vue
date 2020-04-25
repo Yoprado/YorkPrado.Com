594bcb1d66cea92c250c1438fd836f79bde66bb7
York Prado <Yoprado@gmail.com>
2020-04-24 17:38:27 -0700

    initial commit of YorkPrado.com

---
+++ components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link :to="{name: 'index'}" class="logo text-lg">
+        <h1>Y / <span class="secondary-color">P</span></h1>
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .secondary-color {
+    color: #FF1F5F
+  }
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 70px;
+    font-weight: lighter;
+    letter-spacing: 0.5px;
+  }
+  .nav .logo {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .nav .logo .subheader {
+    opacity: 0.70;
+    font-size: 0.9rem;
+    color: white;
+  }
+  .nav .nav-content {
+    font-size: 1rem;
+  }
+  .nav-content ul {
+    display: flex;
+    list-style: none;
+  }
+  .nav-content ul li {
+    padding: 2px 10px;
+  }
+  .nav-content ul li:not(:first-of-type) {
+    border-left: 1px solid rgb(255, 255, 255, 0.2);
+  }
+</style>
