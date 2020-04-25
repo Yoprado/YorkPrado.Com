1e152f551bce89a5f66a7e0c9ecdc958b1bd969a
York Prado <Yoprado@gmail.com>
2020-04-24 21:35:14 -0700

    finished navbar, but may have some issues in bootstrap

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,23 @@
 <template>
-  <nav class="nav">
-    <div class="logo">
-      <nuxt-link :to="{name: 'index'}" class="logo text-lg">
-        <h1>Y / <span class="secondary-color">P</span></h1>
+<div>
+  <b-navbar toggleable="lg">
+    <b-navbar-brand class="logo">
+      <nuxt-link :to="{name: 'index'}" class="navbarBrand">
+        Y/<span class="secondary-color">P</span>
       </nuxt-link>
-    </div>
-  </nav>
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse" right></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto text-decoration-none" right>
+          <nuxt-link class="linkly" :to="{name: 'index'}">About Me</nuxt-link>
+          <nuxt-link :to="{name: 'index'}" class="linkly">Projects</nuxt-link>
+          <nuxt-link :to="{name: 'index'}" class="linkly">Family Recipes</nuxt-link>
+          <nuxt-link :to="{name: 'index'}" class="linkly">Photography</nuxt-link>
+          <nuxt-link :to="{name: 'index'}" class="linkly">Resume</nuxt-link>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -13,38 +25,79 @@ export default {}
 </script>
 
 <style scoped>
-  .secondary-color {
-    color: #FF1F5F
+.navbarBrand{
+  color: var(--gray);
+  text-decoration: none !important;
+}
+.navbarBrand:hover{
+  color: var(--pink);
+  text-decoration: none !important;
+}
+.navbarBrand .secondary-color:hover{
+  color: var(--gray);
+  text-decoration: none !important;
+}
+.linkly {
+  padding-top: .6rem;
+  padding-bottom: .6rem;
+  padding-left: .6rem;
+  padding-right: .6rem;
+  text-align: right;
+  text-decoration: none !important;
+  color: var(--gray);
+}
+.linkly:hover{
+  padding: .5rem .5rem .5rem .5rem;
+  border: .1rem solid var(--pink);
+  border-radius: 25px;
+}
+.ml-auto {
+  letter-spacing: 0.1em;
+  font-weight: 600;
+  font-family: "Avenir Black";
+  font-size: 1em;
+  text-decoration: none !important;
+  color: var(--gray);
+}
+.nav-item {
+  text-align: right;
+}
+@media screen and (max-width: 991px) {
+  .linkly {
+    border-right: 0px !important;
   }
-  nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 70px;
-    font-weight: lighter;
-    letter-spacing: 0.5px;
-  }
-  .nav .logo {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  .nav .logo .subheader {
-    opacity: 0.70;
-    font-size: 0.9rem;
-    color: white;
-  }
-  .nav .nav-content {
-    font-size: 1rem;
-  }
-  .nav-content ul {
-    display: flex;
-    list-style: none;
-  }
-  .nav-content ul li {
-    padding: 2px 10px;
-  }
-  .nav-content ul li:not(:first-of-type) {
-    border-left: 1px solid rgb(255, 255, 255, 0.2);
-  }
+}
+.navbar-nav .linkly:not(:last-child) {
+}
+.logo {
+  font-size: 5rem;
+  color: var(--gray);
+  font-weight: 700;
+}
+.secondary-color {
+  color: #ff1f5f;
+}
+.nav .logo {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.nav .logo .subheader {
+  opacity: 0.7;
+  font-size: 0.9rem;
+  color: white;
+}
+.nav .nav-content {
+  font-size: 1rem;
+}
+.nav-content ul {
+  display: flex;
+  list-style: none;
+}
+.nav-content ul li {
+  padding: 2px 10px;
+}
+.nav-content ul li:not(:first-of-type) {
+  border-left: 1px solid rgb(255, 255, 255, 0.2);
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/mainStyle.css'
   ],
   /*
   ** Plugins to load before mounting the App
