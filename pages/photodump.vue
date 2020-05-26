7004686e5b7f7bb5a23689f50028554d886743e6
York Prado <Yoprado@gmail.com>
2020-05-25 22:09:30 -0700

    made header a component, need to push to other pages. starting on algolia project

--- pages/photodump.vue
+++ pages/photodump.vue
@@ -1,5 +1,5 @@
 <template>
-  <Header :header-text="header"> </Header>
+  <Header :header-text="header" :lead-text="leadText"> </Header>
 </template>
 
 <script>
@@ -9,6 +9,8 @@ export default {
   data() {
     return {
       header: 'Photo Dump Project',
+      leadText:
+        "Using my own mobile photos taken throughout the years, I ran them through Google's Vision API to label my photos and its contents using computer vision. I also extracted EXIF data from each photo to get me location and date of each photo taken. Using all this information, I loaded it into Algolia's Search Engine to easily search through my photos. Feel free to search through my photo collection, they are a mix of concerts, food, loving friends, and scenery.",
     };
   },
 };
